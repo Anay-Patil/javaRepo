f3c51d4ad081cb7087aef5d8a58d743b73851640
Anay Patil <73343302+Anay-Patil@users.noreply.github.com>
2023-04-11 15:29:24 +0530

    Add files via upload

---
+++ Micro-Project/MicroP.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+class MicroP
+{
+	public static void main(String args[])
+	{
+		int ch;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1)Check-In\n2)Check-Out\n3)Exit")
+		System.out.print("\nEnter Your Choice:");
+		ch = sc.nextInt();
+		
+		switch(ch)
+		{
+			case 1: 
+			break;
+			case 2:
+			break;
+		  //case 3:
+	      //break;
+		}
+	}
+}
---
+++ Micro-Project/hotel/checkIN.java
@@ -0,0 +1,84 @@
+package hotel;
+import java.util.*;
+public class checkIN
+{
+	public void single_room()
+	{
+		int adhar;
+		String name,add,ref_id;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your reference id (Alphanumeric)");
+		ref_id = sc.next();
+		System.out.println("Enter your name:");
+		name = sc.next();
+		System.out.println("Enter your Address:");
+		add = sc.nextLine();
+		System.out.println("Enter Valid Adhar number");
+		adhar = sc.nextInt();
+		
+		System.out.println("Hello "+name+" your Single Room is almost booked! Please confirm your transaction. Your Transaction ID: ACRAF23DB3C4");
+		System.out.println("Total Price (incl taxes): Rs.12000");
+		System.out.println("Thank-You for Choosing the Hyatt Regency, we provide the best.");
+		
+	}
+	
+	public void deluxe_room()
+	{
+		int adhar;
+		String name,add,ref_id;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your reference id (Alphanumeric)");
+		ref_id = sc.next();
+		System.out.println("Enter your name:");
+		name = sc.next();
+		System.out.println("Enter your Address:");
+		add = sc.nextLine();
+		System.out.println("Enter Valid Adhar number");
+		adhar = sc.nextInt();
+		
+		System.out.println("Hello "+name+" your Deluxe Room is almost booked! Please confirm your transaction. Your Transaction ID: TQRAF49XC3C4");
+		System.out.println("Total Price (incl taxes): Rs.17000");
+		System.out.println("Thank-You for Choosing the Hyatt Regency, we provide the best.");
+		
+	}
+	
+	public void suite()
+	{
+		int adhar;
+		String name,add,ref_id;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your reference id (Alphanumeric)");
+		ref_id = sc.next();
+		System.out.println("Enter your name:");
+		name = sc.next();
+		System.out.println("Enter your Address:");
+		add = sc.nextLine();
+		System.out.println("Enter Valid Adhar number");
+		adhar = sc.nextInt();
+		
+		System.out.println("Hello "+name+" your Suite is almost booked! Please confirm your transaction. Your Transaction ID: ACRAF23DB3C4");
+		System.out.println("Total Price (incl taxes): Rs.20000");
+		System.out.println("Thank-You for Choosing the Hyatt Regency, we provide the best.");	
+	}
+	
+	public void pre_suite()
+	{
+		int adhar;
+		String name,add,ref_id;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your reference id (Alphanumeric)");
+		ref_id = sc.next();
+		System.out.println("Enter your name:");
+		name = sc.next();
+		System.out.println("Enter your Address:");
+		add = sc.nextLine();
+		System.out.println("Enter Valid Adhar number");
+		adhar = sc.nextInt();
+		
+		System.out.println("Hello "+name+" your Presidential Suite is almost booked! Please confirm your transaction. Your Transaction ID: ACRAF23DB3C4");
+		System.out.println("Total Price (incl taxes): Rs.37000");
+		System.out.println("Thank-You for Choosing the Hyatt Regency, we provide the best.");	
+	}
+	
+	
+}
